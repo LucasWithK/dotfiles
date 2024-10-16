d5877af223c936e6c5c4a9a3baecaee583e83618
IllusionaryFrog <git@illusionaryfrog.com>
2024-10-16 13:59:14 +0000

    added wheel users to trusted users

--- configuration.nix
+++ configuration.nix
@@ -17,4 +17,5 @@
   system.stateVersion = "24.05"; # Did you read the comment?
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "@wheel" ];
 }
